e710be5e4b93b0394c21a247364f09fb77fb7b6f
Jared Ledvina <jared@techsmix.net>
2021-12-01 08:27:38 -0500

    Day 1 - Complete
    Signed-off-by: Jared Ledvina <jared@techsmix.net>

--- day1/day1.py
+++ day1/day1.py
@@ -1,16 +1,45 @@
 #!/usr/bin/env python3
 
+from itertools import islice
+
 def read_input():
     with open('input.txt') as f:
         puzzle_input = f.read().splitlines()
     puzzle_input = [int(entry) for entry in puzzle_input]
-    return sorted(puzzle_input)
+    return puzzle_input
 
 def part1(puzzle_input):
-    pass
+    """
+    >>> part1([199, 200, 208, 210, 200, 207, 240, 269, 260, 263])
+    7
+    """
+    larger = 0
+    previous = False
+    for current in puzzle_input:
+        if previous:
+            if int(previous) < int(current):
+                larger += 1
+        previous = current
+    return larger
+
 
 def part2(puzzle_input):
-    pass
+    """
+    >>> part2([199, 200, 208, 210, 200, 207, 240, 269, 260, 263])
+    5
+    """
+    larger = 0
+    result = tuple(islice(puzzle_input, 3))
+    previous = False
+    for elem in puzzle_input:
+        result = result[1:] + (elem,)
+        current = sum(result)
+        if previous:
+            if previous < current:
+                larger += 1
+        previous = current
+    return larger
+
 
 def main():
     puzzle_input = read_input()
