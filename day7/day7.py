bf93cbea1a08e87e2d4a045a93d344ebd93ada1f
Jared Ledvina <jared@techsmix.net>
2021-12-07 21:57:20 -0500

    Day 7 DONE, its not quick but it gets the job done
    Signed-off-by: Jared Ledvina <jared@techsmix.net>

--- day7/day7.py
+++ day7/day7.py
@@ -3,6 +3,8 @@
 Advent of Code - Day 7
 """
 import os
+import statistics
+from collections import Counter, defaultdict
 
 def read_input():
     """
@@ -11,19 +13,39 @@ def read_input():
     input_path = os.path.join(os.path.dirname(__file__), 'input.txt')
     with open(input_path, encoding='utf-8') as input_file:
         puzzle_input = input_file.read().splitlines()
+    puzzle_input = [int(position) for line in puzzle_input for position in line.split(',')]
     return puzzle_input
 
 
 def part1(puzzle_input):
     """
-    >>> part1([])
+    >>> part1([16,1,2,0,4,2,7,1,2,14])
+    37
     """
+    total_fuel = 0
+    median = statistics.median(puzzle_input)
+    for crab in puzzle_input:
+        total_fuel += abs(crab - median)
+    return int(total_fuel)
 
 
 def part2(puzzle_input):
     """
-    >>> part2([])
+    >>> part2([16,1,2,0,4,2,7,1,2,14])
+    168
     """
+    possible_positions = defaultdict(int)
+    for position in range(min(puzzle_input), max(puzzle_input) + 1):
+        for crab in puzzle_input:
+            number_of_steps = 0
+            if crab != position:
+                total_fuel = 0
+                number_of_steps += abs(crab - position)
+                for fuel in range(1, number_of_steps+1):
+                    total_fuel += fuel
+                possible_positions[position] += total_fuel
+    return possible_positions[min(possible_positions, key=possible_positions.get)]
+
 
 
 def main():
