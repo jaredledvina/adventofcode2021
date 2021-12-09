4844589b3c148b1dadfd6a26576e86b012077876
Jared Ledvina <jared@techsmix.net>
2021-12-08 21:18:33 -0500

    Stage Day 9
    Signed-off-by: Jared Ledvina <jared@techsmix.net>

---
+++ day9/day9.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+Advent of Code - Day 9
+"""
+import os
+
+def read_input():
+    """
+    Reads the puzzle input
+    """
+    input_path = os.path.join(os.path.dirname(__file__), 'input.txt')
+    with open(input_path, encoding='utf-8') as input_file:
+        puzzle_input = input_file.read().splitlines()
+    return puzzle_input
+
+
+def part1(puzzle_input):
+    """
+    >>> part1([])
+    """
+
+
+def part2(puzzle_input):
+    """
+    >>> part2([])
+    """
+
+
+def main():
+    """
+    Do eet
+    """
+    puzzle_input = read_input()
+    print(part1(puzzle_input))
+    print(part2(puzzle_input))
+
+if __name__ == '__main__':
+    main()
