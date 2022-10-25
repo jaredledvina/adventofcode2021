29547565caf4a9b2d39649c43b1573a20c465c4c
Jared Ledvina <jared@techsmix.net>
2022-10-25 19:21:34 -0400

    Saving
    Signed-off-by: Jared Ledvina <jared@techsmix.net>

--- day9/day9.py
+++ day9/day9.py
@@ -13,17 +13,75 @@ def read_input():
         puzzle_input = input_file.read().splitlines()
     return puzzle_input
 
+def generate_grid(puzzle_input):
+    """
+    Generates a grid from the input
+    >>> generate_grid(['2199943210', '3987894921', '9856789892', '8767896789', '9899965678'])
+    [[2, 1, 9, 9, 9, 4, 3, 2, 1, 0], [3, 9, 8, 7, 8, 9, 4, 9, 2, 1], [9, 8, 5, 6, 7, 8, 9, 8, 9, 2], [8, 7, 6, 7, 8, 9, 6, 7, 8, 9], [9, 8, 9, 9, 9, 6, 5, 6, 7, 8]]
+    """
+    grid = []
+    for line in puzzle_input:
+        grid.append([int(x) for x in list(line)])
+    return grid
+
 
 def part1(puzzle_input):
     """
-    >>> part1([])
+    >>> part1(['2199943210', '3987894921', '9856789892', '8767896789', '9899965678'])
+    15
     """
+    low_points = 0
+    grid = generate_grid(puzzle_input)
+    for y, row in enumerate(grid):
+        for x, point in enumerate(row):
+            up, down, left, right = -1, -1, -1, -1
+            if x > 0:
+                left = grid[y][x - 1]
+            if x < len(row) - 1:
+                right = grid[y][x + 1]
+            if y > 0:
+                up = grid[y - 1][x]
+            if y < len(grid) - 1:
+                down = grid[y + 1][x]
+            if up != -1 and point >= up:
+                continue
+            if down != -1 and point >= down:
+                continue
+            if left != -1 and point >= left:
+                continue
+            if right != -1 and point >= right:
+                continue
+            low_points += point + 1 
+    return low_points
 
 
 def part2(puzzle_input):
     """
-    >>> part2([])
+    >>> part2(['2199943210', '3987894921', '9856789892', '8767896789', '9899965678'])
+    1134
     """
+    basins = []
+    grid = generate_grid(puzzle_input)
+    for y, row in enumerate(grid):
+        for x, point in enumerate(row):
+            up, down, left, right = -1, -1, -1, -1
+            if x > 0:
+                left = grid[y][x - 1]
+            if x < len(row) - 1:
+                right = grid[y][x + 1]
+            if y > 0:
+                up = grid[y - 1][x]
+            if y < len(grid) - 1:
+                down = grid[y + 1][x]
+            if up != -1 and point >= up:
+                continue
+            if down != -1 and point >= down:
+                continue
+            if left != -1 and point >= left:
+                continue
+            if right != -1 and point >= right:
+                continue
+    return basins 
 
 
 def main():
